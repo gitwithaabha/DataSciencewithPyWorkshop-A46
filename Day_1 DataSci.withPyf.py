1dcb746b237d8766b538ac5914dad19aabadd018
Aabha Lokhande <130590618+gitwithaabha@users.noreply.github.com>
2023-07-12 19:53:20 +0530

    Add files via upload

---
+++ Day_1 DataSci.withPyf.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Aabha Lokhande')
+
+
+# In[2]:
+
+
+a = 20 
+# a = variable
+# = = assignmenty opertator 
+# 20 = data
+
+
+# In[3]:
+
+
+a 
+
+
+# In[4]:
+
+
+print(a)
+#i/o command
+
+
+# In[5]:
+
+
+a = "Aabha"
+a = 'Aabha'
+a = '''Aabha'''
+# " " = string
+print(a)
+
+
+# In[6]:
+
+
+a = "Aabha"
+b = "Lokhande"
+c = a+b #use to concatenate two strings
+print(c)
+
+
+# In[8]:
+
+
+print("Aabha\nLokhande")
+
+
+# In[10]:
+
+
+print("Aabha\tLokhande")
+
+
+# In[11]:
+
+
+a = 20 
+type(a)
+
+
+# In[12]:
+
+
+a = 'Aabha'
+type(a)
+
+
+# In[ ]:
+
+
+
+
